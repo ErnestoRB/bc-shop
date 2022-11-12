405fb35aac08ae230b9e455964922c98cbe22ca1
Ernesto Ramírez <ernestorb_@outlook.com>
2022-11-11 21:13:57 -0600

    Añadir bootstrap y otros archivos importantes sobre la estructura de la pagina

--- src/articulos.php
+++ src/articulos.php
@@ -7,12 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include "util/bootstrap.html" ?>
-    <title>Inicio</title>
+    <title>Panel de artículos</title>
 </head>
 
 <body>
     <?php include "layout/navbar.php" ?>
-    <main id="content"></main>
+    <main id="content">
+    </main>
     <?php include "layout/footer.html" ?>
 </body>
 
---
+++ src/layout/navbar.php
@@ -0,0 +1,6 @@
+<?php require __DIR__ . '/../util/session.php'; ?>
+<nav class="navbar bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand text-white" href="#">BashCrashers shop</a>
+    </div>
+</nav>
