64e826cbb15b9fdbf5d5e6fea4675257a4648fca
Ernesto <ernestorb_@outlook.com>
2022-11-16 16:47:29 -0600

    Arreglar demo gráficas

--- src/show_data.php
+++ src/show_data.php
@@ -1,25 +1,22 @@
 <?php
-    $servidor = 'localhost';
-    $cuenta = 'root';
-    $password = '';
-    $bd = 'clothesdb';
+require "util/database/connection.php";
 
-    $conexion = new mysqli($servidor, $cuenta, $password, $bd);
+$conexion = getConnection();
+if ($conexion->connect_errno) {
+    die('No se pudo conectar');
+}
 
-    if ($conexion -> connect_errno){
-        die('No se pudo conectar');
-    }
-
-    $sql = "SELECT * FROM ventas";
-    $consulta = $conexion -> query($sql);
-    $datos = array();
-    while($elemento = $consulta -> fetch_object()){
-        $datos[] = $elemento;
-    }
+$sql = "SELECT * FROM Ventas";
+$consulta = $conexion->query($sql);
+$datos = array();
+while ($elemento = $consulta->fetch_object()) {
+    $datos[] = $elemento;
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -29,22 +26,24 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <title>Datos de ventas</title>
 </head>
+
 <body>
     <h1>Prendas vendidas por mes</h1>
     <div class="graphs">
-    <?php
-        if(isset($_POST['bars'])){
+        <?php
+        if (isset($_POST['bars'])) {
             require 'graphs/bars.php';
         }
-        if(isset($_POST['doughnut'])){
+        if (isset($_POST['doughnut'])) {
             require 'graphs/pie.php';
         }
-    ?>
+        ?>
         <form method="post">
             <input type="submit" value="Ver grafica de barras" name="bars">
             <input type="submit" value="Ver grafica de pastel" name="doughnut">
         </form>
-        
+
     </div>
 </body>
+
 </html>
