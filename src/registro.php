7fe2ad4c350d87b796c8936707a0ad3fd5e95dc5
Itzel <karenitzelv42221@gmail.com>
2022-11-18 16:16:13 -0600

    Creacion reg-articulos

--- src/registro.php
+++ src/registro.php
@@ -15,7 +15,13 @@
     <main class="content">
     <h3>Create an acount</h3>
   <div>
+    
     <form style="background:gray" class="form">
+    <div style="align-items: left;">
+    <h6>Already have an account?</h6>
+    </div>
+    
+
     <div class="col-md-6">
     <label for="inputName" class="form-label">First Name</label>
     <input type="text" class="form-control" id="inputName" >
@@ -29,36 +35,44 @@
     <input type="email" class="form-control" id="inputEmail4">
   </div>
   <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
+   <label for="inputPassword4" class="form-label">Password</label>
     <input type="password" class="form-control" id="inputPassword4">
-    <button type="button" class="btn btn-outline-dark">Show</button>
-  </div>
-
+    
+  
+</div>
+  
   <div class="col-md-6">
     <label for="fecha" class="form-label">Birthdate</label>
     <input type="date" class="form-control" id="fecha">
   </div>
   <div class="col-md-6">
-  <input class="form-check-input" type="checkbox" value="" id="flexCheck1">
-  <label class="form-check-label" for="flexCheck1">
-    Recive offers from our parthners
-  </label>
+    <div class="col-2">
+      <div class="form-check ">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheck1">
+      </div>
+    <div class="col-4">
+      <label class="form-check-label" for="flexCheck1">Recive offers from our parthners</label>
+    </div>
+</div>
+  <div class="col-2">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheck2">
+    </div>
+    <label class="form-check-label" for="flexCheck2">Customer data privacy</label>
   </div>
-  <div class="col-md-6">
-  <input class="form-check-input" type="checkbox" value="" id="flexCheck2">
-  <label class="form-check-label" for="flexCheck2">
-    Customer data privacy
-  </label>
-  </div>
-  <div class="col-md-6">
-  <input class="form-check-input" type="checkbox" value="" id="flexCheck3">
-  <label class="form-check-label" for="flexCheck3">
-    Sign up for our newsletter
-  </label>
-  </div>
-    </form>
-  </div>
-    </main>
+  <div class="col-2">
+    <div class="form-check">
+      
+      <input class="form-check-input" type="checkbox" value="" id="flexCheck3">
+    </div>
+    
+    <label class="form-check-label" for="flexCheck3">Sign up for our newsletter</label>
+  
+   </div>
+   </div>
+  </form>
+ </div>
+</main>
     
     <?php include "layout/footer.html"; ?>
 </body>
