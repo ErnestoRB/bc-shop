696a10303bcdfa1fa4f61c2810d7f69d3b15dfca
paulina2906 <al217114@edu.uaa.mx>
2022-11-20 22:14:48 -0600

    index/login

--- src/index.php
+++ src/index.php
@@ -12,7 +12,143 @@
 
 <body>
     <?php include "layout/navbar.php" ?>
-    <main id="content"></main>
+    <main id="content">
+
+    <div class="justify-content-center d-flex mx-0">
+
+        <div class="justify-content-center row mx-0 col-10">
+            
+            <!-- carrusel -->
+            <div id="carouselExampleIndicators" class="carousel slide my-5" data-bs-ride="true">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="assets/carrousel.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+</div>
+            
+        <div class="justify-content-center row mx-0 text-center">
+            <h3>Products</h3>
+        </div>
+        <div class="justify-content-center row mx-0">
+            <div class="col-10 flex-wrap d-flex">
+
+                <!-- tarjetas -->
+                <div class="col my-3 d-flex justify-content-center">
+                    <div class="card" style="width: 12rem;">
+                        <img src="assets/skirt.png" class="card-img-top border-bottom" alt="..." style="box-shadow: 0px 4px 20px 0px #d1d1d1;" >
+                        <div class="card-body text-center">
+                            <div>
+                                <label for="" class="text-muted" >coral skirt</label>
+                            </div>
+                            <div>
+                                <label for="" class="fw-bold">$19.99</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col my-3 d-flex justify-content-center">
+                    <div class="card" style="width: 12rem;">
+                        <img src="..." class="card-img-top border-bottom" alt="..." style="box-shadow: inset 0px -4px 7px 0px #b7b7b7;" >
+                        <div class="card-body text-center">
+                            <div>
+                                <label for="" class="text-muted" >coral skirt</label>
+                            </div>
+                            <div>
+                                <label for="" class="fw-bold">$19.99</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col my-3 d-flex justify-content-center">
+                    <div class="card" style="width: 12rem;">
+                        <img src="..." class="card-img-top border-bottom" alt="..." style="box-shadow: inset 0px -4px 7px 0px #b7b7b7;" >
+                        <div class="card-body text-center">
+                            <div>
+                                <label for="" class="text-muted" >coral skirt</label>
+                            </div>
+                            <div>
+                                <label for="" class="fw-bold">$19.99</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col my-3 d-flex justify-content-center">
+                    <div class="card" style="width: 12rem;">
+                        <img src="..." class="card-img-top border-bottom" alt="..." style="box-shadow: inset 0px -4px 7px 0px #b7b7b7;" >
+                        <div class="card-body text-center">
+                            <div>
+                                <label for="" class="text-muted" >coral skirt</label>
+                            </div>
+                            <div>
+                                <label for="" class="fw-bold">$19.99</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col my-3 d-flex justify-content-center">
+                    <div class="card" style="width: 12rem;">
+                        <img src="..." class="card-img-top border-bottom" alt="..." style="box-shadow: inset 0px -4px 7px 0px #b7b7b7;" >
+                        <div class="card-body text-center">
+                            <div>
+                                <label for="" class="text-muted" >coral skirt</label>
+                            </div>
+                            <div>
+                                <label for="" class="fw-bold">$19.99</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col my-3 d-flex justify-content-center">
+                    <div class="card" style="width: 12rem;">
+                        <img src="..." class="card-img-top border-bottom" alt="..." style="box-shadow: inset 0px -4px 7px 0px #b7b7b7;" >
+                        <div class="card-body text-center">
+                            <div>
+                                <label for="" class="text-muted" >coral skirt</label>
+                            </div>
+                            <div>
+                                <label for="" class="fw-bold">$19.99</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+        <div class="justify-content-center row mx-0">
+
+        </div>
+        <div class="justify-content-center row mx-0">
+
+        </div>
+    </main>
     <?php include "layout/footer.html" ?>
 </body>
 
--- src/login.php
+++ src/login.php
@@ -8,11 +8,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include "util/bootstrap.html" ?>
     <title>Login</title>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 
 <body>
     <?php include "layout/navbar.php" ?>
-    <main id="content"></main>
+    <main id="content">
+        
+        <div class="d-flex align-items-center px-5 py-4">
+            <h2>Login</h2>
+        </div>
+        <div class="d-flex align-items-center justify-content-center mt-5">
+            <div class="card col-8">
+            <div class="card-body pt-4">
+                <div class="justify-content-center d-flex flex-wrap">
+                    <div class="mb-3 col-xs-12 col-sm-10 col-md-6">
+                        <label for="inputEmail" class="form-label fw-bold">Email</label>
+                        <input type="email" class="form-control" id="inputEmail">
+                    </div>
+                </div>
+                <div class="justify-content-center d-flex flex-wrap">
+                    <div class="mb-3 col-xs-12 col-sm-10 col-md-6 ">
+                        <label for="inputPassword" class="form-label fw-bold">Password</label>
+                        <input type="password" class="form-control" id="inputPassword">
+                    </div>
+                </div>
+                <div class="justify-content-center d-flex flex-wrap">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="rememberme">
+                        <label class="form-check-label" for="rememberme">
+                            Remember my password
+                        </label>
+                    </div>
+                </div>
+                <div class="justify-content-center d-flex py-3">
+                    <div class="g-recaptcha" data-sitekey="llave del sitio"></div>
+                </div>
+                <div class="justify-content-center d-flex">
+                    <a class="text-decoration-none text-info" href="">Forgot your password?</a>
+                </div>
+                <div class="justify-content-center d-flex mt-3">
+                    <button type="button" class="btn btn-info text-white">Sign in</button>
+                </div>
+                <hr>
+                <div class="justify-content-center d-flex">
+                    <a class="text-decoration-none text-info" href="">No account ? Create one here</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    
+    </main>
     <?php include "layout/footer.html" ?>
 </body>
 
