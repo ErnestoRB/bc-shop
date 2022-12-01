6e157d1fe18fa2d8e4990a872af461e0208495b2
Itzel <karenitzelv42221@gmail.com>
2022-11-30 22:49:35 -0600

    src/orden.php

---
+++ src/historden.php
@@ -0,0 +1,52 @@
+<?php include_once "util/session.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once "util/bootstrap.html" ?>
+
+    <title>Historial | Pumped Up KickShop</title>
+</head>
+
+<body>
+    <main id="content">
+    <table class="table">
+  <thead>
+    <tr>
+      <th  scope="col">Referencia de pedido</th>
+      <th  scope="col">Fecha</th>
+      <th  scope="col">Precio Total</th>
+      <th  scope="col">Pago</th>
+      <th  scope="col">Estatus</th>
+      <th  scope="col">Factura</th>
+      <th  scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    
+    <tr>
+      <th >CLAVEPEDIDO</th>
+      <td >26 MAR 2022</td>
+      <td >$1299.00</td>
+      <td >Forma_Pago</td>
+      <td >Validaciones</td>
+      <td >--</td>
+      <td ><a href="#" class="text-decoration-none">Detalles</a></td>
+    </tr>
+    <tr>
+      <th >CLAVEPEDIDO</th>
+      <td >27 oct 2022</td>
+      <td >$1299.00</td>
+      <td >Forma_Pago</td>
+      <td >Validaciones</td>
+      <td >--</td>
+      <td ><a href="#" class="text-decoration-none">Detalles</a></td>
+    </tr>
+  </tbody>
+</table>
+
+    </main>
+</body>
---
+++ src/orden.php
@@ -0,0 +1,29 @@
+<?php include_once "util/session.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once "util/bootstrap.html" ?>
+
+    <title>Orden | Pumped Up KickShop</title>
+</head>
+<body>
+  <main id="content">
+    <div id="cont" style="background-color:white; border: 1px solid black;">
+
+    jhghn
+    </div>
+    <div id="csec" style="background-color:white;">
+
+    jhghn
+    </div>
+    <div id="cthr" style="background-color:white;">
+
+    jhghn
+    </div>
+
+  </main>
+</body>
