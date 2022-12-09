b739e52766a939eb855632cd3545de3c55fc48d3
Itzel <karenitzelv42221@gmail.com>
2022-12-09 10:33:57 -0600

    src/images/cupon1.jpg

--- src/index.php
+++ src/index.php
@@ -38,30 +38,69 @@
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Anterior</span>
+                        <span class="visually-hidden">Previous</span>
                     </button>
                     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Siguiente</span>
+                        <span class="visually-hidden">Next</span>
                     </button>
                 </div>
             </div>
         </div>
 
         <div class="justify-content-center row mx-0 text-center">
-            <h3>Últimos productos</h3>
+            <h3>Products</h3>
         </div>
-        <div id="contprod" class="container d-flex flex-wrap">
+        <div id="contprod" class="d-flex flex-wrap">
             <!--Contenedor para ajax-->
             <div class="flex-wrap d-flex">
+
+
+
+
+
+
+
             </div>
         </div>
         <div class="justify-content-center row mx-0">
-
         </div>
         <div class="justify-content-center row mx-0">
 
         </div>
+        <div class="justify-content-center d-flex mx-0">
+
+            <div class="justify-content-center row mx-0 col-10">
+
+                <!-- carrusel -->
+                <div id="carouselExampleIndicators" class="carousel slide my-5" data-bs-ride="true">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    </div>
+                    <div class="carousel-inner">
+
+                        <div class="carousel-item active">
+                            <img src="/images/LLEGAMOS10.jpg" class="d-block w-100" alt="">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/images/QUINCEOFF.jpg" class="d-block w-100" alt="">
+                        </div>
+
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+
     </main>
     <?php include "layout/footer.html" ?>
 </body>
