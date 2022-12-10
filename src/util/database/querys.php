d7ec3de95000f5f3326a1b7e33dcc099190ed5cf
Ernesto Ramírez <ernestorb_@outlook.com>
2022-12-10 15:00:21 -0600

    Hotfix: no limitar productos por categoria

--- src/util/database/querys.php
+++ src/util/database/querys.php
@@ -157,7 +157,7 @@ function getLatestProducts()
 
 function getProductsByCategory()
 {
-    return "SELECT idProducto, p.nombre, c.nombre as categoria, descripcion, existencia, precio, imagen FROM productos as p JOIN categoria as c on c.idCategoria = p.idCategoria WHERE p.idCategoria = ? ORDER BY agregado LIMIT 5";
+    return "SELECT idProducto, p.nombre, c.nombre as categoria, descripcion, existencia, precio, imagen FROM productos as p JOIN categoria as c on c.idCategoria = p.idCategoria WHERE p.idCategoria = ? ORDER BY agregado";
 }
 
 function getProductsFromVenta()
